acc86d8f9a1190ccdb9e4247ac17a441524142aa
veronicavicario <vernik23@gmail.com>
2017-05-24 00:45:47 -0300

    swagger integration

--- src/main/java/com/medi/app/App.java
+++ src/main/java/com/medi/app/App.java
@@ -1,37 +1,43 @@
 package com.medi.app;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
+import sun.applet.Main;
 
 
-public class App
-{
+public class App {
 
+    static final String API_PATH_SPEC = "/api/*";
 
-    public static void main( String[] args ) {
-
+    public static void main( String[] args ) throws Exception {
+        final int port = 8080;
+        final Server server = new Server(port);
         ResourceConfig config = new ResourceConfig();
         config.packages("com.medi.app");
         config.register(JacksonFeature.class);
-        ServletHolder servlet = new ServletHolder(new ServletContainer(config));
+        ServletHolder apiServlet = new ServletHolder(new ServletContainer(config));
 
+        // setup Application context
+        ServletContextHandler context = new ServletContextHandler();
+        apiServlet.setInitOrder(1);
+        context.addServlet(apiServlet, API_PATH_SPEC);
+        // setup static (Swagger UI) resources
 
-        Server server = new Server(2222);
-        ServletContextHandler context = new ServletContextHandler(server, "/*");
-        context.addServlet(servlet, "/*");
-
-
-        try {
-            server.start();
-            server.join();
-        } catch (Exception e) {
-        } finally {
-            server.destroy();
-        }
+        String resourceBasePath = Main.class.getResource(
+                "/swagger").toExternalForm();
+        context.setResourceBase(resourceBasePath);
+        context.setWelcomeFiles(new String[] { "index.html" });
+        ServletHolder swaggerUiServlet = new ServletHolder(new DefaultServlet());
+        swaggerUiServlet.setInitOrder(2);
+        context.addServlet(new ServletHolder(new DefaultServlet()), "/*");
+        server.setHandler(context);
+        server.start();
+        server.join();
     }
 }
+
